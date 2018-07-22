910d8b1f848392f24998c0d10ba006d259c1be2a
niksync1 <40718182+niksync1@users.noreply.github.com>
2018-07-22 19:45:52 +0000

    node practice

--- to-do list/js/to do.js
+++ to-do list/js/to do.js
@@ -5,16 +5,31 @@ var taskList = document.getElementById("taskList")
 addButton.addEventListener("click", function(){
      var task = taskInput.value;
     //Dont add an empty string
-    if(task.trim()){
+    if (task.trim()){
         //add new task list item
         var newItem = document.createElement("li");
         var taskText = document.createTextNode(task);
         newItem.appendChild(taskText);
         //clear text input
         taskInput.value="";
+        //add remove option for new item
+        var removeButton = document.createElement("button");
+        removeButton.innerHTML = "Done";
+        removeButton.className = "remove";
+        removeButton.addEventListener("click",removeTask);
+        newItem.appendChild(removeButton)
         taskList.appendChild(newItem);
     }
     else{
         alert("task cannot be empty")
     }
 });
+var clear = document.getElementById("clear")
+clear.addEventListener("click",function(){
+taskList.innerHTML = ""});
+//remove task from list
+function removeTask(e){
+    //get the parent list item to remove
+    var taskItem = e.target.parentElement;
+    taskList.removeChild(taskItem)
+}
